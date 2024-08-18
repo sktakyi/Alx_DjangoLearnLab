da1acb6eefe4c6e595f61cc784ee7428d5ce1f7d
sktakyi <s.k.takyi5@gmail.com>
2024-08-18 22:59:13 +0000

    Initial Commit

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# Define the Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
+# Define the Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
+    
+    def __str__(self):
+        return self.title
+git add django-models;git commit -m "Initial Commit"
+# Define the Library model
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
+    
+    def __str__(self):
+        return self.name
 
+# Define the Librarian model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
