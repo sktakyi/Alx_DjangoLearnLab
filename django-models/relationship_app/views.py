bd0c9c5cceb4c28e3e394c8b01251ebbdca7eb49
sktakyi <s.k.takyi5@gmail.com>
2024-08-21 21:12:19 +0000

    Intial Commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 # relationship_app/views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
