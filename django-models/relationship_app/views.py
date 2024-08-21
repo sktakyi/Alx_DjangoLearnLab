1de2776a10d4995d13f56f70f847dc20d476f6a2
sktakyi <s.k.takyi5@gmail.com>
2024-08-21 21:21:05 +0000

    Initial Commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -52,16 +52,30 @@ def delete_book(request, pk):
 
 
 # Helper functions for role checks
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
 # Admin View
