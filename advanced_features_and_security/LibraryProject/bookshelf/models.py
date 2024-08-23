bc93f58749143ac2438f53daccf410bc8db7a617
sktakyi <s.k.takyi5@gmail.com>
2024-08-23 00:43:38 +0000

    Initial Commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -28,22 +28,67 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(username, email, date_of_birth, profile_photo, password, **extra_fields)        
 
 
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    pages = models.IntegerField()
+    cover = models.ImageField(upload_to='covers/')
+    language = models.CharField(max_length=20)
 
     class Meta:
         permissions = [
-            ("can_view", "Can view model"),
-            ("can_create", "Can create model"),
-            ("can_edit", "Can edit model"),
-            ("can_delete", "Can delete model"),
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
         ]
 
-  
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render
+    def __str__(self):
+        return self.title
 
-@permission_required('app_name.can_edit', raise_exception=True)
-def edit_view(request, pk):
-    # Logic for editing the model instance
-    return render(request, 'edit_template.html')
+
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'book_form.html', {'form': form})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book_form.html', {'form': form})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'book_confirm_delete.html', {'book': book})
