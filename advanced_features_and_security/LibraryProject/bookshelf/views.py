d2c22850f7a6e72dce91505b6657e48f6ccf8e00
sktakyi <s.k.takyi5@gmail.com>
2024-08-23 02:39:00 +0000

    Initial Commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -8,5 +8,17 @@ def book_list(request):
     books = Book.objects.all()  # Retrieve all book objects from the database
     return render(request, 'book_list.html', {'books': books})
 
+from django.shortcuts import render, redirect
+from .forms import ExampleForm
+
+def book_create(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'book_form.html', {'form': form})
 
 
