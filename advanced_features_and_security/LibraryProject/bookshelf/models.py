9fd369386482054e81fd6f1f29db24aa4c7d26c5
sktakyi <s.k.takyi5@gmail.com>
2024-08-23 00:46:21 +0000

    Initial Commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -92,3 +92,49 @@ def book_delete(request, pk):
         book.delete()
         return redirect('book_list')
     return render(request, 'book_confirm_delete.html', {'book': book})
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm
+
+# List all books
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Fetch all book instances from the database
+    return render(request, 'book_list.html', {'books': books})
+
+# Create a new book
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'book_form.html', {'form': form})
+
+# Edit an existing book
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book_form.html', {'form': form})
+
+# Delete a book
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'book_confirm_delete.html', {'book': book})
