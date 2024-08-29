af985807745ad05be6b1cb8c624c14b0cf767ad9
Samuel Takyi <s.k.takyi5@gmail.com>
2024-08-30 03:36:00 +1000

    Initial Commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Defining view to list all books.
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
