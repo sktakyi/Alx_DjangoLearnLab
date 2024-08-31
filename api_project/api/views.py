b904e311db6e315a456b8c432dc25eb82a8ed24e
Samuel Takyi <s.k.takyi5@gmail.com>
2024-09-01 01:05:32 +1000

    Initial Commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet  # Import ModelViewSet directly
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(rest_framework.generics.ListAPIView):
+# BookList for listing books
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# BookViewSet for full CRUD operations
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
