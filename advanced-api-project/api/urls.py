3e9004fc2dcd740ec9e0c6d61acfd57c95c950ff
Samuel Takyi <s.k.takyi5@gmail.com>
2024-09-07 15:34:39 +1000

    Initial Commit

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.contrib import admin
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from django.urls import path, include
 
 urlpatterns = [
+     path('admin/', admin.site.urls),  # Django Admin
+    path('api/', include('api.urls')),  # Include the api app's URLs under the 'api/' path
     path('books/', BookListView.as_view(), name='book-list'),  # List all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # View a single book by ID
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
